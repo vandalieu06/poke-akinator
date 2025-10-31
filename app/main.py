4f001f06f4985c8e890c55714ee40d4d15c0d432
Jhonny Vicmar Claure Vedia <claurejhonnny188@gmail.com>
2025-10-31 10:50:43 +0100

    update

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import requests
 import json
 import os
+import random
 
 
-class PokeAPi:
+class PokeAPI:
     POKE_API_BASE_URI = "https://pokeapi.co/api/v2/pokemon/"
 
     def __init__(self):
@@ -24,49 +25,67 @@ class PokeAPi:
         extract_evolution_names(evos_data, evolution_list)
         return evolution_list
 
-    def get_poke_info(self, poke_index=1) -> dict:
+    def _get_poke_base_data(self, poke_index=1) -> dict:
         poke_res = requests.get(os.path.join(self.POKE_API_BASE_URI, str(poke_index)))
         poke_data = poke_res.json()
 
-        name = poke_data["name"]
-        image = poke_data["sprites"]["back_default"]
-        weight_kg = poke_data["weight"] / 10  # HG a KG
-        types = [p["type"]["name"] for p in poke_data["types"]]
+        return {
+            "name": poke_data["name"],
+            "image": poke_data["sprites"]["front_default"],
+            "weight_kg": poke_data["weight"] / 10,  # HG a KG
+            "types": [p["type"]["name"] for p in poke_data["types"]],
+            "species_url": poke_data["species"]["url"],
+        }
 
-        specie_data = self._request_data(poke_data["species"]["url"])
+    def _get_species_data(self, species_url):
+        specie_data = self._request_data(species_url)
+        return {
+            "color": specie_data["color"]["name"],
+            "is_baby": specie_data["is_baby"],
+            "is_legendary": specie_data["is_legendary"],
+            "is_mythical": specie_data["is_mythical"],
+            "varieties_name": [v["pokemon"]["name"] for v in specie_data["varieties"]],
+            "evolution_chain_url": specie_data["evolution_chain"]["url"],
+        }
 
-        color = specie_data["color"]["name"]
-        is_baby = specie_data["is_baby"]
-        is_legendary = specie_data["is_legendary"]
-        is_mythical = specie_data["is_mythical"]
-        varieties_name = [v["pokemon"]["name"] for v in specie_data["varieties"]]
+    def get_poke_info(self, poke_index=1) -> dict:
+        pokemon_base_data = self._get_poke_base_data(poke_index)
+        species_url = pokemon_base_data.pop("species_url")
 
-        evos_data = self._request_data(specie_data["evolution_chain"]["url"])
-        evos = self._get_evolution_names(evos_data["chain"])
+        species_data = self._get_species_data(species_url)
+        evolution_chain_url = species_data.pop("evolution_chain_url")
+
+        evos_data = self._request_data(evolution_chain_url)
+        evolution_names = self._get_evolution_names(evos_data["chain"])
 
         pokemon_info = {
-            "name": name,
-            "image": image,
-            "weight_kg": weight_kg,
-            "types": types,
-            "color": color,
-            "is_baby": is_baby,
-            "is_legendary": is_legendary,
-            "is_mythical": is_mythical,
-            "varieties": varieties_name,
-            "evos": evos,
+            **pokemon_base_data,
+            **species_data,
+            "evos": evolution_names,
         }
+
         return pokemon_info
 
 
-poke = PokeAPi()
-poke_info = poke.get_poke_info(777)
-print(json.dumps(poke_info, indent=2))
+poke = PokeAPI()
 
-file_name = os.path.join(os.getcwd(),'app/data/data.json')
+POKEDEX_NUM = 1025
+POKE_NUM = 30
+poke_ids = random.sample(range(1, POKEDEX_NUM + 1), POKE_NUM)
+poke_list = [poke.get_poke_info(id) for id in poke_ids]
 
-with open(file_name, 'w') as data_json:
-    json.dump(poke_info, data_json, indent = 4)
+# while len(poke_ids) < 30:
+#     num = random.randint(1, 1025)
+#     if num not in poke_ids:
+#         poke_ids.append(num)
+#         poke_list.append(poke.get_poke_info(num))
+
+
+file_name = os.path.join(os.getcwd(), "app/data/data.json")
+
+with open(file_name, "w") as data_json:
+    json.dump(poke_list, data_json, indent=2)
+
 
 # https://www.datacamp.com/es/tutorial/python-private-methods-explained?dc_referrer=https%3A%2F%2Fwww.google.com%2F
 # Codigo Anterior de obtener las veoluciones de manera statica
