fd2bfa16b7405abecbd087b1bb33c20012639fe8
Guillem Fonts <gufofe@jviladoms.cat>
2025-11-04 10:58:01 +0100

    update

--- app/main.py
+++ app/main.py
@@ -1,11 +1,30 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+import json
+import os
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/", methods = ["GET"])
+def index():
+    ruta_json = os.path.join(app.root_path, "data", "data.json")
+    
+    with open(ruta_json, "r", encoding="utf-8") as f:
+        pokemons = json.load(f)
+
+    return render_template("index.html", pokemons=pokemons)
+
+@app.route('/preguntas', methods=['GET', 'POST'])
+def preguntas():
+    if request.method == 'POST':
+        respuesta = request.form.get('respuesta')
+
+    return render_template('preguntas.html')
+
+@app.route('/resultado')
+def resultado():
+    pokemon = request.args.get('pokemon', 'Desconocido')
+    return render_template('resultado.html', pokemon=pokemon)
 
 
 if __name__ == "__main__":
