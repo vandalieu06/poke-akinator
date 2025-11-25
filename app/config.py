340d2e4d98ff8e9ee28424336b77731694bcdc5c
Jhonny Claure <claurejhonny188@gmail.com>
2025-11-26 00:49:33 +0100

    update

--- app/config.py
+++ app/config.py
@@ -1,20 +1,14 @@
 class Config(object):
-    """Configuración base común a todos los entornos."""
-
     SECRET_KEY = "86d27b0aaa812eee1b0d607355b1eaf96c4ebd955cc4f72523543535a109b671"
     MAIN_REDIRECT_URL = "/game"
 
 
 class DevelopmentConfig(Config):
-    """Configuración para el entorno de Desarrollo."""
-
     DEBUG = True
     HOST = "127.0.0.1"
     PORT = 5000
 
 
 class ProductionConfig(Config):
-    """Configuración para el entorno de Despliegue/Producción."""
-
     DEBUG = False
     HOST = "0.0.0.0"
--- app/main.py
+++ app/main.py
@@ -6,7 +6,4 @@ if __name__ == "__main__":
     app.run(host=app.config["HOST"], port=app.config["PORT"])
 
 # Para Despliegue
-# Cuando se despliega (por ejemplo, con Gunicorn o un WSGI server),
-# se llama a 'app' directamente. Por ejemplo: gunicorn run:app
-# La configuración de ProductionConfig se cargaría cambiando el parámetro:
 # app = create_app('config.ProductionConfig')
